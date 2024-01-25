bab850524d7695d0f0b397f566be1b96dc44314e
six-shot <okhuomonajayi54@gmail.com>
2024-01-25 13:17:35 +0100

    mobile header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,20 @@
 "use client";
-import React from 'react'
-import { IconContext } from 'react-icons';
+import React from "react";
+import { IconContext } from "react-icons";
 import * as RiIcons from "react-icons/ri";
 import * as CiIcons from "react-icons/ci";
 import * as IoIcons from "react-icons/io5";
 import * as GoIcons from "react-icons/go";
-export default function Header () {
+export default function Header() {
   return (
-    <header className="w-full">
-      <IconContext.Provider value={{ size: "20px" }}>
-        <div className="flex justify-between items-center ">
-          <div>
-            <RiIcons.RiMenu2Line />
-          </div>
+    <header className="w-full ">
+      <div className="flex justify-between items-center ">
+        <div>
+          <IconContext.Provider value={{ size: "25px" }}>
+            <RiIcons.RiMenu2Line className="text-[30px]" />
+          </IconContext.Provider>
+        </div>
+        <IconContext.Provider value={{ size: "20px" }}>
           <div className="flex items-center gap-1">
             <div className="p-1.5 border-[1px] border-black rounded-[8px]">
               <CiIcons.CiSearch />
@@ -23,12 +25,10 @@ export default function Header () {
             <div className="p-1.5 border-[1px] border-black rounded-[8px]">
               <GoIcons.GoBell />
             </div>
-            <div className="w-[50px] h-[40px] py-2 bg-black rounded-[10px]">
-       
-            </div>
+            <div className="w-[50px] h-[40px] py-2 bg-black rounded-[10px]"></div>
           </div>
-        </div>
-      </IconContext.Provider>
+        </IconContext.Provider>
+      </div>
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="px-[5%] pt-[4%]">
-      <Header/>
-     
+    <main className="px-[5%] bg-[#242529] w-full h-[100vh] pt-[4%]">
+      <Header />
     </main>
   );
 }
