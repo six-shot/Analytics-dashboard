b0a5e2ef7d642feacc29d02447a3545e600a02ad
six-shot <okhuomonajayi54@gmail.com>
2024-01-25 15:48:21 +0100

    mobile header

--- app/components/ThemeSwitch.tsx
+++ app/components/ThemeSwitch.tsx
@@ -13,24 +13,31 @@ export default function ThemeSwitch() {
 
   if (!mounted)
     return (
-      <div className="p-1.5 border-[1px] border-rgba(255,255,255,0.7) rounded-[8px]">
-        <Image
-          src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-          width={36}
-          height={36}
-          sizes="36x36"
-          alt="Loading Light/Dark Toggle"
-          priority={false}
-          title="Loading Light/Dark Toggle"
-        />
-      </div>
+      <Image
+        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+        width={36}
+        height={36}
+        sizes="36x36"
+        alt="Loading Light/Dark Toggle"
+        priority={false}
+        title="Loading Light/Dark Toggle"
+      />
     );
 
   if (resolvedTheme === "dark") {
-    return <FiSun onClick={() => setTheme("light")} />;
+    return (
+      <div className="p-1.5 border-[1px] border-rgba(255,255,255,0.7) rounded-[8px]">
+        <FiSun onClick={() => setTheme("light")} />
+      </div>
+    );
   }
 
   if (resolvedTheme === "light") {
-    return <FiMoon onClick={() => setTheme("dark")} />;
+    return (
+      <div className="p-1.5 border-[1px] border-rgba(255,255,255,0.7) rounded-[8px]">
+        {" "}
+        <FiMoon onClick={() => setTheme("dark")} />{" "}
+      </div>
+    );
   }
 }
