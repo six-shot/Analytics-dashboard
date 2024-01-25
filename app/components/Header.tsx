195477f112f3ad53ef15a89cf7f7fd791d8d6f11
six-shot <okhuomonajayi54@gmail.com>
2024-01-25 13:00:44 +0100

    mobile header

---
+++ app/components/Header.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from 'react'
+import { IconContext } from 'react-icons';
+import * as RiIcons from "react-icons/ri";
+import * as CiIcons from "react-icons/ci";
+import * as IoIcons from "react-icons/io5";
+import * as GoIcons from "react-icons/go";
+export default function () {
+  return (
+    <header className="w-full">
+      <IconContext.Provider value={{ size: "30px" }}>
+        <div className="flex justify-between items-center ">
+          <div>
+            <RiIcons.RiMenu2Line />
+          </div>
+          <div className="flex items-center gap-1">
+            <div className="p-1.5 border-[1px] border-black rounded-[8px]">
+              <CiIcons.CiSearch />
+            </div>
+            <div className="p-1.5 border-[1px] border-black rounded-[8px]">
+              <IoIcons.IoMoonOutline />
+            </div>
+            <div className="p-1.5 border-[1px] border-black rounded-[8px]">
+              <GoIcons.GoBell />
+            </div>
+            <div className="w-[50px] h-[50px] py-2 bg-black rounded-[10px]">
+       
+            </div>
+          </div>
+        </div>
+      </IconContext.Provider>
+    </header>
+  );
+}
