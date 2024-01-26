5667143f550d9343c2a1f72b581110f0f02cd210
six-shot <okhuomonajayi54@gmail.com>
2024-01-26 05:23:03 +0100

    bottom right

--- app/components/BottomRight.tsx
+++ app/components/BottomRight.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BottomRight() {
   return (
     <div className="w-full bg-white border border-[#EDF2F7] p-[5%] rounded-xl mt-4 ">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mb-2">
         <h5 className="font-semibold text-[#26282C] font-plus_jakara_sans">
           Top Platform
         </h5>
@@ -11,23 +11,27 @@ export default function BottomRight() {
           See All
         </h5>
       </div>
-      <div className="flex gap-5 flex-col">
+      <div className="flex gap-3 flex-col">
         <div className="skill-box relative">
+          <h5 className="mb-1 font-plus_jakara_sans text-sm">Book Bazaar</h5>
           <div className="skill-bar ">
             <span className="skill-per html bg-[#6160DC]"></span>
           </div>
         </div>
         <div className="skill-box">
-          <div className="skill-bar relative">
+          <h5 className="mb-1 font-plus_jakara_sans text-sm">Artisan Aisle</h5>
+          <div className="skill-bar ">
             <span className="skill-per css bg-[#54C5EB]"></span>
           </div>
         </div>
         <div className="skill-box">
+          <h5 className="mb-1 font-plus_jakara_sans text-sm">Toy Troop</h5>
           <div className="skill-bar">
             <span className="skill-per javascript bg-[#FFB74A]"></span>
           </div>
         </div>
         <div className="skill-box">
+          <h5 className="mb-1 font-plus_jakara_sans text-sm">XStore</h5>
           <div className="skill-bar">
             <span className="skill-per nodejs bg-[#FF4A55]"></span>
           </div>
