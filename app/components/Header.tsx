0039cc46582bdf7e85e6279555f54de1b77dd9f4
six-shot <okhuomonajayi54@gmail.com>
2024-01-26 02:58:23 +0100

    top right

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,28 +14,30 @@ export default function Header() {
     <header className="w-full h-[80px] px-[4%] pt-[2%]">
       <div className="flex items-center w-full justify-between ">
         <div className="flex gap-2 items-center">
-          <h5 className=" text-[18px]">MENU</h5>
-          <IconContext.Provider value={{ size: "25px" }}>
-            <HiIcons.HiMenuAlt4 className="text-[30px]" />
+          {/* <h5 className=" text-[18px]">MENU</h5> */}
+          <IconContext.Provider value={{ size: "25px",color:'white' }}>
+            <div className="bg-black p-2 rounded-[8px]">
+              <HiIcons.HiMenuAlt4 />
+            </div>
           </IconContext.Provider>
         </div>
-         <IconContext.Provider value={{ size: "20px",  }}>
-        <div className="flex items-center gap-1">
-          <div className="p-1.5 border-[1px] border-rgba(255,255,255,0.7) rounded-[8px]">
-            <CiIcons.CiSearch />
+        <IconContext.Provider value={{ size: "20px" }}>
+          <div className="flex items-center gap-1">
+            <div className="p-1.5 border-[1px] border-rgba(255,255,255,0.7) rounded-[8px]">
+              <CiIcons.CiSearch />
+            </div>
+            <div className="p-1.5 border-[1px] border-rgba(255,255,255,0.7) rounded-[8px]">
+              <GoIcons.GoBell />
+            </div>
+            <ThemeSwitch />
+            <div className="w-[50px] h-[40px]  bg-black rounded-[10px]">
+              <Image
+                src={man}
+                className="w-full h-full object-cover rounded-[10px]"
+                alt="man"
+              />
+            </div>
           </div>
-          <div className="p-1.5 border-[1px] border-rgba(255,255,255,0.7) rounded-[8px]">
-            <GoIcons.GoBell />
-          </div>
-          <ThemeSwitch />
-          <div className="w-[50px] h-[40px]  bg-black rounded-[10px]">
-            <Image
-              src={man}
-              className="w-full h-full object-cover rounded-[10px]"
-              alt="man"
-            />
-          </div>
-        </div>
         </IconContext.Provider>
 
         {/* <IconContext.Provider value={{ size: "20px", color: "black" }}>
