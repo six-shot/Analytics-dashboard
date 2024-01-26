63756cbe661afb53dbf856be5d91f8e1a510d395
six-shot <okhuomonajayi54@gmail.com>
2024-01-26 17:05:52 +0100

    bottom right

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,84 +1,100 @@
 import Image from "next/image";
 import logo from "@/public/assets/Logo.svg";
 import React from "react";
-import {
-  MdDashboard,
-  MdSupervisedUserCircle,
-  MdShoppingBag,
-  MdAttachMoney,
-  MdWork,
-  MdAnalytics,
-  MdPeople,
-  MdOutlineSettings,
-  MdHelpCenter,
-  MdLogout,
-} from "react-icons/md";
+import dashboard from "@/public/assets/dashboard.svg";
+import setting from "@/public/assets/setting.svg";
+import logout from "@/public/assets/logout.svg";
+import arrowright from "@/public/assets/arrow-right.svg";
+import trendup from "@/public/assets/trend-up.svg";
+import profile from "@/public/assets/profile-2user.svg";
+import info from "@/public/assets/info-circle.svg";
+import discount from "@/public/assets/discount-shape.svg";
+import box from "@/public/assets/box.svg";
 
 import MenuLink from "./menuLink";
 
 const menuItems = [
   {
-  
-    list: [
-      {
-        title: "Dashboard",
-        path: "/dashboard",
-        icon: <MdDashboard />,
-      },
-      {
-        title: "Users",
-        path: "/dashboard/users",
-        icon: <MdSupervisedUserCircle />,
-      },
-      {
-        title: "Products",
-        path: "/dashboard/products",
-        icon: <MdShoppingBag />,
-      },
-      {
-        title: "Transactions",
-        path: "/dashboard/transactions",
-        icon: <MdAttachMoney />,
-      },
-    ],
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: dashboard,
+  },
+  {
+    title: "Users",
+    path: "/dashboard/users",
+    icon: trendup,
+  },
+  {
+    title: "Products",
+    path: "/dashboard/products",
+    icon: profile,
   },
  
   {
-    title: "User",
-    list: [
-      {
-        title: "Settings",
-        path: "/dashboard/settings",
-        icon: <MdOutlineSettings />,
-      },
-      {
-        title: "Help",
-        path: "/dashboard/help",
-        icon: <MdHelpCenter />,
-      },
-    ],
+    title: "Users",
+    path: "/dashboard/users",
+    icon: box,
   },
+  {
+    title: "Products",
+    path: "/dashboard/products",
+    icon: discount,
+  },
+  {
+    title: "Transactions",
+    path: "/dashboard/transactions",
+    icon: info,
+  }
+ 
+];
+const menuItemsbottom = [
+  {
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: arrowright,
+  },
+  {
+    title: "Users",
+    path: "/dashboard/users",
+    icon: setting,
+  },
+  {
+    title: "Products",
+    path: "/dashboard/products",
+    icon: logout,
+  },
+ 
+  
+ 
 ];
 export default function Sidebar() {
   return (
     <div>
       <div className="h-screen justify-between  w-full flex  flex-col items-center  ">
-        
-          <div className="py-5">
-            <Image src={logo} alt="logo" />
-          </div>
-         
-        
-        <ul className="flex justify-between h-full flex-col">
-          {menuItems.map((cat) => (
-            <li key={cat.title} className="flex gap-6 flex-col">
-              
-              {cat.list.map((item) => (
-                <MenuLink item={item} key={item.title} />
+        <div className="py-5">
+          <Image src={logo} alt="logo" />
+        </div>
+
+        <div className="flex flex-col justify-between mt-5 h-full">
+          <div className="h-[60%]  ">
+            <ul className="flex gap-8 h-full   flex-col">
+              {menuItems.map((cat) => (
+                <li key={cat.title} className="flex gap-6  flex-col">
+                  <Image src={cat.icon} alt="icon" />
+                </li>
               ))}
-            </li>
-          ))}
-        </ul>
+            </ul>
+          </div>
+          <div className="h-[30%]  ">
+            <ul className="flex gap-8 h-full   flex-col">
+              {menuItemsbottom.map((catt) => (
+                <li key={catt.title} className="flex gap-6  flex-col">
+                  <Image src={catt.icon} alt="icon" />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
