d21d501c234cf2c770eee1a769e9df14dded0754
six-shot <okhuomonajayi54@gmail.com>
2024-01-27 12:31:18 +0100

    theme

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
   return (
     <header>
       <div className="sm:hidden flex">
-        <div className="w-full fixed top-0 z-10 bg-white  h-[100px] px-[4%] py-[2%]">
+        <div className="w-full fixed top-0 z-10 bg-white dark:bg-[#161619]  h-[100px] px-[4%] py-[2%]">
           <div className="flex items-center w-full justify-between ">
             <div className="flex gap-2 items-center">
               {/* <h5 className=" text-[18px]">MENU</h5> */}
