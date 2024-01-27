91e1ca091a0c7c5316d083ef2a4f44cbfb0f7f98
six-shot <okhuomonajayi54@gmail.com>
2024-01-27 14:43:31 +0100

    theme

--- app/components/Chart.tsx
+++ app/components/Chart.tsx
@@ -78,40 +78,42 @@ const data = [
 
 export default function Chart() {
   return (
-    <div className="h-[22rem] bg-white dark:bg-[#161619] dark:border dark:border-[#161619] py-4 font-plus_jakara_sans rounded-xl border border-gray-200 flex flex-col flex-1">
+    
+    <div className="h-[22rem] overflow-x-auto bg-white dark:bg-[#161619] dark:border dark:border-[#161619] py-4 font-plus_jakara_sans rounded-xl border border-gray-200 flex flex-col flex-1">
+      
       <h5 className="font-semibold text-[#26282C] dark:text-[#EDF2F6] font-plus_jakara_sans px-[4%] pb-3">
         Sales Trend
       </h5>
-      <div className="mt-3 w-full flex-1 text-xs">
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 0,
-              right: 0,
-              left: -10,
-              bottom: 0,
-            }}
-            barSize={27}
-          >
-            <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip
-              content={<CustomTooltip />}
-              cursor={{ fill: "transparent" }}
-            />
+ 
+        <div className="mt-3  flex-1 text-xs   sm:w-full w-[800px] ">
+          <ResponsiveContainer >
+            <BarChart
+              data={data}
+              margin={{
+                top: 0,
+                right: 0,
+                left: -10,
+                bottom: 0,
+              }}
+              barSize={27}
+            >
+              <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip
+                content={<CustomTooltip />}
+                cursor={{ fill: "transparent" }}
+              />
 
-            <Bar
-              dataKey="Expense"
-              fill="rgba(52, 202, 165, 0.10)"
-              radius={[20, 20, 0, 0]}
-            />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
+              <Bar
+                dataKey="Expense"
+                fill="rgba(52, 202, 165, 0.10)"
+                radius={[20, 20, 0, 0]}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+  
     </div>
   );
 }
