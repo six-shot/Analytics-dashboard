a8e2bb822804a262cd0c7bf49f6504b173c154e6
six-shot <okhuomonajayi54@gmail.com>
2024-01-29 17:21:44 +0100

    chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar'
 
 export default function page() {
   return (
-    <div>
+    <div className='mx-auto max-w-[1440px]'>
       <div className="flex ">
         <div className="sm:flex hidden">
           <div className="w-[80px] fixed  border-r-[1px] border-[#989bac] bg-[#F7F8FA] dark:border-[#34CAA5] dark:bg-black ">
