4633f504d2f73bb0c1e9576a438f57875cc0c0fb
six-shot <okhuomonajayi54@gmail.com>
2024-02-09 19:23:46 -0800

    Update layout.tsx

--- app/dashboardd/layout.tsx
+++ app/dashboardd/layout.tsx
@@ -8,13 +8,15 @@ const Layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div className="flex">
-      <div className=" sm:flex hidden w-[80px] border-r-[1px] border-[#989bac] bg-[#F7F8FA] dark:border-[#34CAA5] dark:bg-black  ">
-        <Sidebar />
-      </div>
-      <div className=" w-full bg-[#f6f6f6]">
-        <Header />
-        {children}
+    <div className="mx-auto max-w-[1440px]">
+      <div className="flex">
+        <div className=" w-[80px] border-r-[1px] border-[#989bac] bg-[#F7F8FA] dark:border-[#34CAA5] dark:bg-black  ">
+          <Sidebar />
+        </div>
+        <div className=" w-full bg-[#f6f6f6]">
+          <Header />
+          {children}
+        </div>
       </div>
     </div>
   );
