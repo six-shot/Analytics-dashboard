f2ccd59b5c2b82b53d7ca984947c71d6efd5fce9
six-shot <okhuomonajayi54@gmail.com>
2024-02-10 04:02:45 +0100

    Update layout.tsx

--- app/dashboardd/layout.tsx
+++ app/dashboardd/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({
 }>) => {
   return (
     <div className="flex">
-      <div className=" w-[80px] border-r-[1px] border-[#989bac] bg-[#F7F8FA] dark:border-[#34CAA5] dark:bg-black  ">
+      <div className=" sm:flex hidden w-[80px] border-r-[1px] border-[#989bac] bg-[#F7F8FA] dark:border-[#34CAA5] dark:bg-black  ">
         <Sidebar />
       </div>
       <div className=" w-full bg-[#f6f6f6]">
